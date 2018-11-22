5d85f9f200b5707820317da3aa17bb3da4547668
NFenner <fennern@myumanitoba.ca>
2018-11-22 07:14:24 -0600

    Implement click to seek behaviour

--- watch/player.js
+++ watch/player.js
@@ -1,6 +1,16 @@
 var vid = document.getElementById("vidplayer");
 var ovl = document.getElementById("vidOverlay");
 
+var wasPaused = true;
+var progressDrag = false;
+
+ovl.addEventListener("mouseover",
+	function addFadeOutClass(e) {
+		ovl.classList.add("vidOverlayFadeOut");
+		ovl.removeEventListener("mouseover", addFadeOutClass);
+	}
+);
+
 document.getElementById("playButton").addEventListener("click",
 	function(e) {
 		var btnIcon = e.currentTarget.getElementsByTagName("img")[0];
@@ -15,6 +25,46 @@ document.getElementById("playButton").addEventListener("click",
 	}
 );
 
+document.getElementById("progressBar").addEventListener("mousedown",
+	function(e) {
+		wasPaused = vid.paused;
+		vid.pause();
+		progressDrag = true;
+
+		{
+			var bar = document.getElementById("progressBar")
+			var barLeft = bar.getBoundingClientRect().left;
+
+			e.preventDefault();
+			vid.currentTime =
+				vid.duration * (e.clientX - barLeft) / bar.clientWidth;
+		}
+	}
+);
+
+document.addEventListener("mousemove",
+	function(e) {
+		if (progressDrag) {
+			var bar = document.getElementById("progressBar")
+			var barLeft = bar.getBoundingClientRect().left;
+
+			e.preventDefault();
+			vid.currentTime =
+				vid.duration * (e.clientX - barLeft) / bar.clientWidth;
+		}
+	}
+);
+
+document.addEventListener("mouseup",
+	function(e) {
+		if (!wasPaused) {
+			vid.play();
+			wasPaused = true;
+		}
+		progressDrag = false;
+	}
+);
+
 vid.addEventListener("ended",
 	function(e) {
 		var btnIcon = ovl.querySelector("#playButton > img");
@@ -22,3 +72,8 @@ vid.addEventListener("ended",
 		btnIcon.src = "playbutton1.svg";
 	}
 );
+
+function progressUpdate() {
+	document.getElementById("progressFill").style.width = (100.0 * vid.currentTime / vid.duration).toString() + "%";
+}
+window.setInterval(progressUpdate, 100);
