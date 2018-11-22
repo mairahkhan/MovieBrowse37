0fc02d440ea9b55583c120d92f0108bf4c182eae
NFenner <fennern@myumanitoba.ca>
2018-11-22 04:15:39 -0600

    Implement play and pause

--- watch/player.js
+++ watch/player.js
@@ -1,4 +1,16 @@
 var vid = document.getElementById("vidplayer");
-var cnv = document.getElementById("cnv");
-var ctx = document.getContext("2d");
+var ovl = document.getElementById("vidOverlay");
 
+document.getElementById("playButton").addEventListener("click",
+	function(e) {
+		var btnIcon = e.currentTarget.getElementsByTagName("img")[0];
+
+		if (vid.paused) {
+			vid.play();
+			btnIcon.src = "pausebutton1.svg";
+		} else {
+			vid.pause();
+			btnIcon.src = "playbutton1.svg";
+		}
+	}
+);
